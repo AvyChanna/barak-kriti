71f1cc7447c760a0ffa0a3836598f633b3fe0504
AvyChanna <30595749+AvyChanna@users.noreply.github.com>
2019-10-20 22:41:07 +0530

    Added search

--- aspire/home/views.py
+++ aspire/home/views.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from django.shortcuts import render, get_object_or_404, Http404
 from django.http import HttpResponseRedirect
 from aspire.home.models import Department, Course, Video, Book, Novel, Note
@@ -5,6 +6,13 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404 , Http404
 from django.http import HttpResponseRedirect, HttpResponse
 
+=======
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.shortcuts import Http404, get_object_or_404, render
+
+from aspire.home.models import Book, Course, Department, Novel, Video
+>>>>>>> Stashed changes
 
 def index(request):
     return HttpResponseRedirect("accounts/login")
@@ -14,28 +22,40 @@ def home(request):
     d = Department.objects.all()
     return render(request, "home/index.html", {'depts':d})
 
-
-
 @login_required
 def courses(request, dept, sub):
-    d = get_object_or_404(Department, slug=dept)
     c = get_object_or_404(Course, slug=sub)
     if c.department.slug != dept:
         raise Http404
+    d = get_object_or_404(Department, slug=dept)
+    t = c.tag_set.all()
     v = Video.objects.filter(course=c)
     b = Book.objects.filter(course=c)
-    n = Note.objects.filter(course =c)
-    return render(request, "course/index.html", {'dept':d, 'course':c, 'videos':v, 'books':b , 'notes':n})
-
+    return render(request, "course/index.html",
+                  {'dept':d, 'course':c, 'videos':v, 'books':b, "tags": t})
 
 @login_required
 def departments(request, slug):
     d = get_object_or_404(Department, slug=slug)
     c = Course.objects.filter(department=d)
-    return render(request, 'department/index.html', {'dept': d, "courses":c})
+    t = c.tag_set.all()
+    return render(request, 'department/index.html', {'dept':d, "courses":c, "tags":t})
 
 @login_required
 def bookshare(request):
     n = Novel.objects.all()
     return render(request, "bookshare/index.html", {'novels':n})
 
+<<<<<<< Updated upstream
+=======
+@login_required
+def search(request):
+    if request.method != 'GET':
+        return Http404
+    if request.GET.get('q', '') == '':
+        return HttpResponseRedirect(status=304, "/home/")
+    q = request.GET.get('q', '')
+    v = Video.objects.filter(title=q)
+    b = Book.objects.filter(title=q)
+    return render(request, 'search/index.html', {"videos":v, "books":b} )
+>>>>>>> Stashed changes

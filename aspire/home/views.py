eeb237f6370ea828aadf7b46bc7227fe4c04b6c4
sjain24 <jain170107062>
2019-10-20 16:27:00 +0530

    video and book subsection added

--- aspire/home/views.py
+++ aspire/home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404 , Http404
 from django.http import HttpResponseRedirect, HttpResponse
 from aspire.home.models import Department, Course, Video, Book
 
@@ -16,13 +16,13 @@ def courses(request, dept, sub):
     d = get_object_or_404(Department, slug=dept)
     c = get_object_or_404(Course, slug=sub)
     if c.department.slug != dept:
-        return HttpResponse("Invalid Course/Department", status=404)
+        raise Http404
     v = Video.objects.filter(course=c)
     b = Book.objects.filter(course=c)
     return render(request, "course/index.html", {'dept':d, 'course':c, 'videos':v, 'books':b})
 
+
 def departments(request, slug):
     d = get_object_or_404(Department, slug=slug)
     c = Course.objects.filter(department=d)
-    Course.objects.filter(slug)
     return render(request, 'department/index.html', {'dept': d, "courses":c})

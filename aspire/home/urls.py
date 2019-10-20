cac8ab2663bdbce8cfe9ab9d16949aad5a1aac8e
AvyChanna <30595749+AvyChanna@users.noreply.github.com>
2019-10-20 16:30:26 +0530

    Cleaned Repo

--- aspire/home/urls.py
+++ aspire/home/urls.py
@@ -4,6 +4,6 @@ from aspire.home import views
 urlpatterns = [
     path(r'', views.index, name='index'),
     path(r'home/', views.home, name='home'),
+    path(r'depts/<slug:dept>/course/<slug:sub>', views.courses, name="courses"),
     path(r'depts/<slug:slug>', views.departments, name='departments'),
-    path(r'depts/<slug:dept>/course/<slug:sub>', views.courses, name="courses")
 ]
